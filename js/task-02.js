17ea5ddb27ff86ba4615b8fe4968cb6d2878ed63
Vitalii Shulga <v.shulga019@gmail.com>
2021-10-14 12:20:13 +0300

    Updated task-02

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,14 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments']
+
+const listOfIngredients = document.querySelector('#ingredients')
+
+const elements = ingredients.map(ingredient => {
+  const element = document.createElement('li')
+  element.textContent = ingredient
+  element.classList.add('item')
+  return element
+})
+
+listOfIngredients.append(...elements)
+
+console.log(listOfIngredients)
