d7dd776d26c9c402333ae189c4bbce52d9168a72
Vitalii Shulga <v.shulga019@gmail.com>
2021-10-17 23:59:30 +0300

    Updated task-01-02-03

--- js/task-01.js
+++ js/task-01.js
@@ -1,9 +1,3 @@
-/* Напиши скрипт который:
- * Посчитает и выведет в консоль количество категорий в ul#categories, то есть элементов li.item.
- * Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль текст заголовка элемента (тега <h2>)
- * и количество элементов в категории (всех вложенных в него <li>).
- */
-
 const itemEl = document.querySelectorAll('.item')
 
 console.log(`Number of categories: ${itemEl.length}`)
--- js/task-02.js
+++ js/task-02.js
@@ -1,10 +1,3 @@
-/* Напиши скрипт, который для каждого элемента массива ingredients:
- * Создаст отдельный элемент <li>. Обзательно используй метод document.createElement().
- * Добавит название ингредиента как его текстовое содержимое.
- * Добавит элементу класс item.
- * После чего вставит все <li> за одну операцию в список ul.ingredients.
- */
-
 const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments']
 
 const elements = ingredients.map(ingredient => {
