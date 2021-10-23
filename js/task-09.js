2cd144dfb1ef2ff18953cbfadb1496b288a75b4e
Vitalii Shulga <v.shulga019@gmail.com>
2021-10-24 00:13:57 +0300

    Updated task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
+const refs = {
+  body: document.querySelector('body'),
+  color: document.querySelector('.color'),
+  buttonChangeColor: document.querySelector('.change-color'),
+}
+
+refs.buttonChangeColor.addEventListener('click', backgroundChangeColor)
+
+function backgroundChangeColor() {
+  const newColor = getRandomHexColor()
+  refs.body.style.backgroundColor = newColor
+  refs.color.textContent = newColor
+}
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`
 }
