decfc249246c436c4b832a6ca2a54f02b3ecb7da
Vitalii Shulga <v.shulga019@gmail.com>
2021-10-24 00:13:44 +0300

    Updated task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector('.login-form')
 
+form.addEventListener('submit', onFormSubmit)
+
+function onFormSubmit(event) {
+  event.preventDefault()
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget
+
+  const userData = {}
+  userData[email.name] = email.value
+  userData[password.name] = password.value
+  console.log(userData)
+
+  if (email.value === '' || password.value === '') {
+    return alert('Все поля должны быть заполнены')
+  }
+
+  event.currentTarget.reset()
+}
