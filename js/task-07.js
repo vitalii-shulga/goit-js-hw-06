10594305cc7c60402a84c553c49a5c9cb0abcaea
Vitalii Shulga <v.shulga019@gmail.com>
2021-10-24 12:54:11 +0300

    Updated task-07

--- js/task-07.js
+++ js/task-07.js
@@ -1,9 +1,7 @@
-const refs = {
-  inputSlider: document.querySelector('#font-size-control'),
-  text: document.querySelector('#text'),
-}
+const inputSlider = document.querySelector('#font-size-control')
+const text = document.querySelector('#text')
 
-refs.inputSlider.addEventListener('input', onInputSlider)
+inputSlider.addEventListener('input', onInputSlider)
 
 function onInputSlider(event) {
   return (text.style.fontSize = `${event.currentTarget.value}px`)
