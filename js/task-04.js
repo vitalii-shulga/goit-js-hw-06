d168323717b758a5d106de7c7d1c3e77f105e227
Vitalii Shulga <v.shulga019@gmail.com>
2021-10-26 20:57:33 +0300

    Updated task-04

--- js/task-04.js
+++ js/task-04.js
@@ -2,7 +2,7 @@ const value = document.querySelector('#value')
 const decrementBtn = document.querySelector('[data-action="decrement"]')
 const incrementBtn = document.querySelector('[data-action="increment"]')
 
-let counterValue = value.textContent
+let counterValue = 0
 
 decrementBtn.addEventListener('click', onDecrementBtnClick)
 incrementBtn.addEventListener('click', onIncrementBtnClick)
